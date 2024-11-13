5561c2e6038fed58dc8913e048c2d48bf3a00c2a
ShonLibo <Benjaminlibo222@gmail.com>
2024-11-13 18:30:48 +0300

    Commit 5
    In line 11 of the Library Class, the Variable "Lam" stored in the variable name "author" was changed to "Jordan".

--- src/LibraryPackage/Library.java
+++ src/LibraryPackage/Library.java
@@ -8,7 +8,7 @@ import static ApprovalPackage.Approval.BookIdValid;
 
 public class Library {
     public static void main(String[] args) {
-   Book book= new Book("Love and Thunder","Lam", 10);
+   Book book= new Book("Love and Thunder","Jordan", 10);
 
    Visitor visitor = new Visitor("Mick","LamX@gamil.com");
         Borrowing borrowing = new Borrowing("King Jerico","Jack","2023_12_13");
