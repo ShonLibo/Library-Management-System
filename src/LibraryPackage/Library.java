17a35918ad3f822f8cf426b0e05cf2f6440337d8
ShonLibo <Benjaminlibo222@gmail.com>
2024-11-13 18:24:49 +0300

    Commit 2
    in line 14 of the Library Class, the variable "Lam" stored in the variable name "visitor" was changed to "Jack".

--- src/LibraryPackage/Library.java
+++ src/LibraryPackage/Library.java
@@ -11,7 +11,7 @@ public class Library {
    Book book= new Book("Love and Thunder","Lam", 10);
 
    Visitor visitor = new Visitor("Lam","LamX@gamil.com");
-        Borrowing borrowing = new Borrowing("Love and Thunder","Lam","2023_12_13");
+        Borrowing borrowing = new Borrowing("Love and Thunder","Jack","2023_12_13");
 
         System.out.println("Book Details:");
         System.out.println("Title: " + book.getTitle());
